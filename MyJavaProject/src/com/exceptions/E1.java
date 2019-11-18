b27725713fcf10dc1e7e912e4d4b30d3dc06fcca
Suhas <57176178+Suhas2703@users.noreply.github.com>
2019-11-18 10:40:16 +0530

    commit

--- MyJavaProject/src/com/exceptions/E1.java
+++ MyJavaProject/src/com/exceptions/E1.java
@@ -7,7 +7,13 @@ public static void main(String[] args) {
 	System.out.println(1);
 	int b=0;
 	System.out.println(2);
-	//int c=a/b;//exception calling statementS
+	try {
+	int c=a/b;//exception calling statements
+	}
+	catch(ArithmeticException e)
+	{
+		System.out.println("Caught");
+	}
 	System.out.println(3);
 	System.out.println("main ends");
 }
--- MyJavaProject/src/com/exceptions/E2.java
+++ MyJavaProject/src/com/exceptions/E2.java
@@ -6,7 +6,13 @@ public static void main(String[] args) {
 	System.out.println(1);
 	E2 ob=null;
 	System.out.println(2);
-	//System.out.println(ob.i);//null pointer exception
+	try {
+	System.out.println(ob.i);//null pointer exception
+	}
+	catch(NullPointerException e)
+	{
+		System.out.println("Caught");
+	}
 	System.out.println(3);
 }
 }
